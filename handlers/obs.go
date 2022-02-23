60e0500a74099511d7d32dffeb5a767ae87b3d4a
rmil <rhys.milling@ystv.co.uk>
2022-02-23 20:45:45 +0000

    add playout manage page

--- handlers/obs.go
+++ handlers/obs.go
@@ -28,3 +28,17 @@ func (h *Handlers) obsGetPlayout(c echo.Context) error {
 	}
 	return echo.NewHTTPError(http.StatusNotFound)
 }
+
+func (h *Handlers) obsManagePlayout(c echo.Context) error {
+	po, err := h.play.List(c.Request().Context())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	for _, playout := range po {
+		if strconv.Itoa(playout.PlayoutID) == c.Param("playoutID") {
+			return c.Render(http.StatusOK, "manage-playout", playout)
+
+		}
+	}
+	return echo.NewHTTPError(http.StatusNotFound)
+}
