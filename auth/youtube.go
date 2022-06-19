ca52b706b39b69e11cadc434d9ef1452d1f40eb0
rmil <rhys@mill.in.net>
2022-06-19 23:24:17 +0100

    Add temporary debug statement
    ticket-id: SHOW-12

--- auth/youtube.go
+++ auth/youtube.go
@@ -1,6 +1,9 @@
 package auth
 
 import (
+	"fmt"
+	"log"
+
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/youtube/v3"
@@ -8,6 +11,10 @@ import (
 
 // NewYouTubeConfig creates a oauth2 config for YouTube.
 func NewYouTubeConfig(b []byte) (*oauth2.Config, error) {
+	// Temporary debug.
+	log.Println("temporary debug start")
+	fmt.Printf("%+s", string(b))
+	log.Println("temporary debug end")
 	// If modifying these scopes, delete your previously saved credentials file.
 	return google.ConfigFromJSON(b, youtube.YoutubeForceSslScope)
 }
